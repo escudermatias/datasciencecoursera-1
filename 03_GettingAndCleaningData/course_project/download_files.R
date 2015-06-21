f88b76370d09a0ea46fd80a49acdbd6831e094bb
Matt Flor <matthias.c.flor@gmail.com>
2015-06-21 14:23:40 +0200

    Replace download_file.R with get_data.R

--- 03_GettingAndCleaningData/course_project/download_files.R
+++
@@ -1,6 +0,0 @@
-library(data.table)
-library(plyr)
-library(dplyr)
-
-fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileURL, "./data/UCI_Dataset.zip", method = "curl")
